e6e7027341229f0ed686f17d750cd76046f69c6f
Kaue Avila <trabalho.kaue@hotmail.com>
2023-11-09 20:51:49 -0300

    Table instead of text

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,13 +1,13 @@
+import Evento from "@/core/Evento";
 import Layout from "../components/layout/layout";
+import Tabela from "../components/eventos/tabelas";
 export default function Eventos() {
- return (
- <div className={`
- flex justify-center items-center h-screen
- bg-gradient-to-bl from-indigo-900 via-indigo-400 to-indigo-900
- text-white`}>
- <Layout titulo="Cadastro de eventos">
- Conteúdo
- </Layout>
- </div>
- )
+    const eventos = Evento.geraEventosMock()
+    return (
+        <div className={`flex justify-center items-center h-screen bg-gradient-to-bl from-indigo-900 via-indigo-400 to-indigo-900 text-white`}>
+            <Layout titulo="Cadastro de eventos">
+                <Tabela eventos={eventos}></Tabela>
+            </Layout>
+    </div>
+    )
 }
