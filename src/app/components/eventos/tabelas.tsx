be45c89f276b1f99747ab185b9ab50ce7ab6fee6
Kaue Avila <trabalho.kaue@hotmail.com>
2023-11-09 21:02:14 -0300

    Icons implementation

--- src/app/components/eventos/tabelas.tsx
+++ src/app/components/eventos/tabelas.tsx
@@ -1,4 +1,5 @@
 import Evento from "@/core/Evento"
+import { editIcon, trashIcon } from "../icones/tabelas"
 interface TabelaProps {
     eventos: Evento[]
 }
@@ -24,6 +25,7 @@ export default function Tabela(props: TabelaProps) {
                     <th className="text-left p-3">data</th> 
                     <th className="text-left p-3">descricao</th> 
                     <th className="text-left p-3">status</th>
+                    <th className="text-left p-3">ações</th>
              </tr>)
         }
         function renderDados() {
@@ -35,7 +37,21 @@ export default function Tabela(props: TabelaProps) {
                         <td className="text-left p-3">{evento.data}</td>
                         <td className="text-left p-3">{evento.descricao}</td> 
                         <td className="text-left p-3">{evento.status}</td>
+                        {renderizarAcoes(evento)}
                     </tr>)
              })}
+
+        function renderizarAcoes(evento: Evento) {
+            return (
+                <td className="flex">
+                    <button className={`flex justify-center items
+                    text-green-600 rounded-full p-2 m-1
+                    hover:bg-gray-100`}>{editIcon}</button>
+                    <button className={`flex justify-center items
+                    text-red-600 rounded-full p-2 m-1
+                    hover:bg-gray-100`}>{trashIcon}</button>
+                </td>
+            )
+        }
 }
  
